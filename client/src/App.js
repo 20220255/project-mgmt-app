eb41e30dc9a53a02d5fd42492cb1998e11256d2f
20220255 <118017744+20220255@users.noreply.github.com>
2024-09-21 03:34:25 +1200

    2nd test for uri graphql

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ let client;
 
 // if (process.env.NODE_ENV === "production") {
   client = new ApolloClient({
-    uri: "https://project-mgmt-app-ajiz.onrender.com/graphql",
+    uri: "https://project-mgmt-app-ajiz.onrender.com",
     cache,
   });  
 // } else {
